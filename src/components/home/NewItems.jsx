9492436a6b7dca0250eaf61a26bf1196f40d917d
Robin Doirin <robindoirin@gmail.com>
2024-06-02 01:36:23 -0400

    made new items section dynamic and add a countdown timer

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useEffect, useState } from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import axios from "axios";
+import ItemCard from "../UI/ItemCard";
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
+  const [newItems, setNewItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  async function fetchNewItems() {
+    try {
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+      );
+      setNewItems(data);
+      setIsLoaded(true);
+    } catch (error) {
+      console.error("Error fetching new items:", error);
+      setIsLoaded(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchNewItems();
+  }, []);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +36,95 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+          {isLoaded ? (
+            <OwlCarousel
+              className="owl-theme"
+              margin={10}
+              loop
+              items={4}
+              nav
+              dots={false}
+              lazyLoad
+              responsiveClass={true}
+              responsive={{
+                0: {
+                  items: 1,
+                },
+                740: {
+                  items: 2,
+                },
+                1000: {
+                  items: 3,
+                },
+                1400: {
+                  items: 4,
+                },
+              }}
+            >
+              {newItems.map((item) => (
+                <ItemCard key={item.id} item={item} />
+              ))}
+            </OwlCarousel>
+          ) : (
+            <OwlCarousel
+              className="owl-theme"
+              margin={10}
+              loop
+              items={4}
+              nav
+              dots={false}
+              lazyLoad
+              responsiveClass={true}
+              responsive={{
+                0: {
+                  items: 1,
+                },
+                740: {
+                  items: 2,
+                },
+                1000: {
+                  items: 3,
+                },
+                1400: {
+                  items: 4,
+                },
+              }}
+            >
+             {[...Array(4)].map((_, index) => (
+                  <div key={index}>
+                      <div className="author_list_pp">
+                        <Skeleton
+                          width="60px"
+                          height="60px"
+                          alt=""
+                        />
+                      <i className="fa fa-check"></i>
+                      </div>
+                      
+                    <div className="nft__item">
+                      <Skeleton
+                        className="lazy img-fluid"
+                        width="275px"
+                        height="300px"
+                      />
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                      <div className="nft__item_info">
+                        <Skeleton width="160px" height="15px" />
+                        <h4>
+                          <Skeleton width="60px" height="15px" />
+                        </h4>
+                        <div className="nft__item_like">
+                          <i className="fa fa-heart"></i>
+                          <span>
+                            <Skeleton width="30px" height="15px" />
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+                ))}
+            </OwlCarousel>
+          )}
         </div>
       </div>
     </section>
